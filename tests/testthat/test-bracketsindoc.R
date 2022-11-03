ac1b1012c29c656fefd7ee2cd4ada59d5cd3fd05
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2022-11-03 08:47:15 +0100

    run pandoc check locally only, rhub can't handle

--- tests/testthat/test-bracketsindoc.R
+++ tests/testthat/test-bracketsindoc.R
@@ -1,3 +1,5 @@
+# Only run on our local machines, add your sys user if you want
+if(Sys.info()["user"] %in% c("vanlissa")){
 lnz <- c("---", "output            : pdf_document", "---", "", "# Span method",
          "", "<span id = \"spanmethod\">", "", "First line", "", "Second line",
          "", "</span>", "", "", "# MD method", "", "[", "", "First line",
@@ -23,3 +25,4 @@ test_that("rendered tempfile has no brackets", {
   expect_false(grepl("[", tmp, fixed = TRUE))
   expect_false(grepl("]", tmp, fixed = TRUE))
 })
+}
