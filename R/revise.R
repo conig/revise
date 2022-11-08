cdd795bdf47874ef3a5677e6d33127af247e4901
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2022-11-08 07:27:02 +0100

    Add package help file

---
+++ R/revise.R
@@ -0,0 +1,12 @@
+#' revise: Tools for Formulating Revise and Resubmit Rmarkdown Documents
+#'
+#' When writing scientific manuscripts, it is challenging to keep revisions
+#' synchronized between the manuscript and the action letter detailing
+#' responses to reviewers.
+#'
+#' Revise automates this process. Sections in the manuscript can be tagged,
+#' and inserted into the action letter.
+#'
+#' @docType package
+#' @name revise
+NULL
