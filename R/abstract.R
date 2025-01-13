4bd77ba52632b1d284eb2b78b184979c99c93e95
yourname <yourname@email.com>
2025-01-13 14:42:39 +0100

    stop exporting abstract() because it is not documented and not used anywhere

--- R/abstract.R
+++ R/abstract.R
@@ -1,10 +1,10 @@
-#' abstract
-#'
-#' Create a structured abstract including cached results
-#' @param ... headed abstract sections
-#' @param cache_name character indicating the name the cached r chunk
-#' @param sep how should sections be pasted together?
-#' @export
+# abstract
+#
+# Create a structured abstract including cached results
+# @param ... headed abstract sections
+# @param cache_name character indicating the name the cached r chunk
+# @param sep how should sections be pasted together?
+# @export
 
 abstract <- function(..., cache_name = NULL, sep = "\n\n") {
   elip <- list(...)
